5eee7a96f97c529a0b47859d858d0a7a4a61f288
Jeff Emmett <jmemmett@comcast.net>
2018-11-21 16:39:59 -0700

    Most functionality is now working, but I can't get the score banner to display at the top of the page when the submit button has been pressed.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,85 +1,98 @@
-//-------------------------
-// GLOBAL VARIABLES
-//-------------------------
+//------------------------------------
+// Global Variables
+//------------------------------------
 
-    var triviaQuestions = [
-        {"Question": "What's the answer for question 1?", "Answer": "true"},
-        {"Question": "What's the answer for question 2?", "Answer": "false"},
-    ];
-    var correctAnswers = 0;
-    var incorrectAnswers = 0;
     var counterRunning = false;
-    var counter = 11;
+    var counter = 20;
     var timer;
-    
+    var total = 5 // setting the number of questions to 5
+    var answers = ["d", "a", "d", "b", "b"];
+    var q1;
+    var q2;
+    var q3;
+    var q4;
+    var q5;
+    var results;
+    var score = 0;
+    var unanswered = 0; // initializing the score to zero
 
-//-------------------------
-// FUNCTION DEFINITIONS
-//-------------------------
+//------------------------------------
+// Function Definition Declarations
+//------------------------------------
 
+    // Decrements the timer when the quiz is started
     function decrement() {
         counter--;
-        $("#counter").html("<p id='question'>Time remaining to answer these questions: " + counter + "</p>"); // display counter
+        $("#counter").html("<h2 id='question'>Time remaining to answer these questions: " + counter + "</h2>"); // display counter
         if ( counter === 0) {
             clearInterval(timer);
-            alert("Game over!"); // game over
-            // display scores
+            submitAnswers();
         }
     }
 
-    function displayQuestions() {
-        for ( i = 0; i < triviaQuestions.length; i++ ) {
-            // var questions = $("<p>").text(triviaQuestions[i].Question);
-            var label = $("<label name='question-" + i + "'>").html(triviaQuestions[i].Question);
-            var trueLabel = $("<label>").html("true");
-            var falseLabel = $("<label>").html("false");
-            var trueRadio = $("<input type='radio' value='true' name='question-" + i + "'>");
-            var falseRadio = $("<input type='radio' value='false' name='question-" + i + "'>");
-            trueLabel.append(trueRadio);
-            falseLabel.append(falseRadio);
-            label.append(trueLabel).append(falseLabel);
-            // var choices = $("<input type='radio'>").text(triviaQuestions[i].Answer);
-            
-            $("#question").append(label);
-            // $("#question").append(choices);
-            // $("#question").append(triviaQuestions[i]); // display the counter and let it count down from 15 seconds
-            
-            // if ( counter === 0 ) { // if the counter reaches zero
-            //     incorrectAnswers++; // consider the answer to this question to be incorrect
-                
-            // } 
+    // Kicked off when the Submit Answers button is clicked
+    function submitAnswers() {
+        recordAnswers();
+        validation();
+        checkAnswers();
+        displayResults();
+    }
+
+    function recordAnswers() {
+        q1 = document.forms["quizForm"]["q1"].value;
+        q2 = document.forms["quizForm"]["q2"].value;
+        q3 = document.forms["quizForm"]["q3"].value;
+        q4 = document.forms["quizForm"]["q4"].value;
+        q5 = document.forms["quizForm"]["q5"].value;
+    }
+
+    function validation() {
+        if ( counter !== 0 ) {
+            for ( var i = 1; i <= total; i++ ) {
+                if ( eval('q' + i) == null || eval('q' + i) == "" ) {
+                    alert("You missed question " + i);
+                    return false;
+                }
+            }
+        } else {
+            for ( var i = 1; i <= total; i++ ) {
+              // set all unanswered questions to incorrect
+              if ( eval('q' + i) == null ) {
+                  unanswered++; 
+              }
+            }
+        }
+        console.log(unanswered);
+    }
+
+    function checkAnswers() {
+        for ( var i = 1; i <= total; i++ ) {
+            if( eval('q' + i) == answers[i-1]) {
+                score++;
+            } else {
+                return false;
+            }
         }
     }
 
-    function finalScore() {
-
+    function displayResults() {
+        results = $("#results").html("<h3>You scored <span>" + score + "</span> out of <span>" + total + "</span></h3>");
+        alert("You scored " + score + " out of " + total);
+        
+        return false;
     }
 
-//-------------------------
-// GAMEPLAY
-//-------------------------
+//------------------------------------
+// Script / Gameplay
+//------------------------------------
 
-    $("#submit").hide();
+    $("#main").hide();
+    $("#counter").hide();
 
     // when the user clicks the START button
-    $("#start-btn").on("click", function() {
+    $("#start").on("click", function() {
         $("#start").hide(); // remove the start button from the screen
+        $("#main").show(); // show the previous hidden main section containing the questions
+        $("#counter").show(); // show the counter as it counts down
         timer = setInterval(decrement, 1000); // find out why there is a delay before this shows up
-        displayQuestions();
-        $("#submit").show();
-    })
-
-
-
-        // loop through the triviaQuestions array/object
-            // replace the previous question with the new question on the page
-            // display multiple choice or true/false answer options
-            // if user answers the question correctly, correctAnswers++
-            // if user answers the question incorrectly, incorrectAnswers++
-            // if user fails to enter before the timer hits zero, incorrectAnswers++
-            // go back to the top of this loop (unless all questions have been asked/answered
-    // start the timer and display it along with the first question
-        // if the timer hits zero prior to the user answering the question, record their answer as incorrect and move on to the next question
-    // record the answer to each question and use the answer to determine if it was correct or incorrect
-    // the game ends either when the timer runs out or all questions have been answered
-    // at the end of the game, replace any previous content displayed on the screen with the user's score
+    });
